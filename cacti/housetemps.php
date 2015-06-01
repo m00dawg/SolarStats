1ae4b63f02a029930006f1213921c103a73272f0
Tim Soderstrom <tim@moocowproductions.org>
2015-05-31 23:06:49 -0500

    Removed dumbass debugging code

--- cacti/housetemps.php
+++ cacti/housetemps.php
@@ -3,7 +3,7 @@
     $m = new Memcached();
     $m->addServer('localhost', 11211);
     $outsideTemp = 'U';
-    if($m->get('SolarStats:outsideTemperatureBad'))
+    if($m->get('SolarStats:outsideTemperature'))
         $outsideTemp = $m->get('SolarStats:outsideTemperature');
     echo "OutsideTemperature:$outsideTemp";
 ?>
