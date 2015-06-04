9c47681e3fb808f83dbcb7ee47204a298b9e319f
Tim Soderstrom <tim@moocowproductions.org>
2015-06-04 15:56:48 -0500

    Stuff rawr

---
+++ sql/views.sql
@@ -0,0 +1,22 @@
+DROP VIEW IF EXISTS UsageByDay;
+CREATE VIEW UsageByDay AS
+SELECT
+DATE(logDate) AS "Day",
+DAYOFWEEK(logDate) AS "Weekday",
+ROUND(SUM(meterKWH) + SUM(solarKWH), 3) AS "UsedKWH",
+ROUND(SUM(meterKWH), 3) AS "GridKWH",
+ROUND(SUM(solarKWH), 3) AS "SolarKWH",
+ROUND(AVG(outsideTemperature), 2) AS "AvgTemp"
+FROM PowerUsage
+GROUP BY DATE(logDate);
+
+DROP VIEW IF EXISTS AverageUsageByHour;
+CREATE VIEW AverageUsageByHour AS
+SELECT
+HOUR(logDate) AS "Hour",
+ROUND(AVG(meterKWH) + AVG(solarKWH), 3) AS "UsedKWH",
+ROUND(AVG(meterKWH), 3) AS "GridKWH",
+ROUND(AVG(solarKWH), 3) AS "SolarKWH",
+ROUND(AVG(outsideTemperature), 2) AS "AvgTemp"
+FROM PowerUsage
+GROUP BY HOUR(logDate);
