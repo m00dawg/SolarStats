95685c0c5319f57ac31a858a2e307801ece2b0d7
Tim Soderstrom <tim@moocowproductions.org>
2015-06-04 18:10:28 -0500

    Probably should update the views

--- sql/views.sql
+++ sql/views.sql
@@ -6,11 +6,34 @@ DAYOFWEEK(logDate) AS "Weekday",
 ROUND(SUM(meterKWH) + SUM(solarKWH), 3) AS "UsedKWH",
 ROUND(SUM(meterKWH), 3) AS "GridKWH",
 ROUND(SUM(solarKWH), 3) AS "SolarKWH",
-ROUND(AVG(outsideTemperature), 2) AS "AvgTemp"
+ROUND(AVG(outsideTemperature), 2) AS "OutsideTemp"
 FROM PowerUsage
 GROUP BY DATE(logDate);
 
 DROP VIEW IF EXISTS AverageUsageByHour;
+CREATE VIEW AverageUsageByHour AS
+SELECT
+HOUR(logDate) AS "Hour",
+ROUND(AVG(meterKWH) + AVG(solarKWH), 3) AS "UsedKWH",
+ROUND(AVG(meterKWH), 3) AS "GridKWH",
+ROUND(AVG(solarKWH), 3) AS "SolarKWH",
+ROUND(AVG(outsideTemperature), 2) AS "OutsideTemp"
+FROM PowerUsage
+GROUP BY HOUR(logDate);
+
+DROP VIEW IF EXISTS TodaysUsageByHour;
+CREATE VIEW TodaysUsageByHour AS
+SELECT
+HOUR(logDate) AS "Hour",
+ROUND(AVG(meterKWH) + AVG(solarKWH), 3) AS "UsedKWH",
+ROUND(AVG(meterKWH), 3) AS "GridKWH",
+ROUND(AVG(solarKWH), 3) AS "SolarKWH",
+ROUND(AVG(outsideTemperature), 2) AS "OutsideTemp"
+FROM PowerUsage
+WHERE logDate > CONCAT(DATE(NOW(), ' 00:00:00');
+GROUP BY HOUR(logDate);
+
+DROP VIEW IF EXISTS AverageSolarVsGrid;
 CREATE VIEW AverageUsageByHour AS
 SELECT
 HOUR(logDate) AS "Hour",
